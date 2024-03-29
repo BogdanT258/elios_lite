b1c7a9d5d618115fadef0a2d0415347c7a2e04e8
Bogdan <buffalobre@gmail.com>
2024-03-29 19:45:29 +0100

    Post adding to firebase collection

--- elios-lite/src/Pages/AddPost.jsx
+++ elios-lite/src/Pages/AddPost.jsx
@@ -1,13 +1,18 @@
 import React, { useState } from "react";
-import { getStorage, ref, uploadBytes } from "firebase/storage";
+import { getStorage, ref, uploadBytes, getDownloadURL } from "firebase/storage";
 import DefaultLayout from "../Components/DefaultLayout";
 import { useDispatch } from "react-redux";
 import { toast } from "react-toastify";
+import { addDoc, collection } from "firebase/firestore";
+import { firedb } from "../firebaseConfig";
+import { useNavigate } from "react-router-dom";
 
 const AddPost = () => {
   const [image, setImage] = useState("");
+  const [title, setTitle] = useState("");
   const [description, setDescription] = useState("");
-  const dispatch = useDispatch();
+  const dispatch = useDispatch(); 
+  const navigate = useNavigate();
 
   const addPost = () => {
     dispatch({ type: "showLoading" });
@@ -15,9 +20,23 @@ const AddPost = () => {
     const storageRef = ref(storage, `posts/${image.name}`);
     uploadBytes(storageRef, image)
       .then((snapshot) => {
-        console.log("uploaded a blob file");
-        toast.success("Post added");
-        dispatch({ type: "hideLoading" });
+        getDownloadURL(ref(storage, `posts/${image.name}`)).then((url) => {
+          addDoc(collection(firedb, 'posts'), {
+            title,
+            description,
+            imageURL: url,
+            likes: [],
+            comments: [],
+            user: JSON.parse(localStorage.getItem('userData'))
+          }).then(() => {
+            toast.success('Post added successfully')
+            dispatch({ type: "hideLoading" });
+            navigate('/');
+          }).catch(() => {
+            dispatch({ type: "hideLoading" });
+            toast.error('Something went wrong');
+          })
+        })
       })
       .catch((error) => {
         console.error("Error uploading file: ", error);
@@ -42,6 +61,7 @@ const AddPost = () => {
             spellCheck="false"
             placeholder="Title"
             type="text"
+            onChange={(e) => {setTitle(e.target.value)}}
           />
           <textarea
             className="description bg-gray-100 sec p-3 h-60 border border-gray-300 outline-none"
@@ -49,7 +69,7 @@ const AddPost = () => {
             placeholder="Describe everything about this post here"
             onChange={(e) => setDescription(e.target.value)}
           ></textarea>
-  
+
           <div className="icons flex text-gray-500 m-2">
             <label htmlFor="file-input" className="sr-only">
               Choose file
@@ -61,7 +81,7 @@ const AddPost = () => {
               className="block w-full border border-gray-200 shadow-sm rounded-lg text-sm focus:z-10 focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-gray-400 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600 file:bg-gray-50 file:border-0 file:me-4 file:py-3 file:px-4 dark:file:bg-gray-700 dark:file:text-gray-400"
               onChange={(e) => onImageChange(e)}
             />
-  
+
             <div className="count ml-auto text-gray-400 text-xs font-semibold">
               0/300
             </div>
@@ -86,17 +106,17 @@ const AddPost = () => {
           </div>
         </div>
         {image && (
-        <div className="w-1/2">          
+          <div className="w-1/2">
             <article className="p-6 bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700 m-5 max-w-2xl">
               <div className="mx-auto max-w-screen-sm text-center lg:mb-16 mb-8">
                 <h1 className="mb-4 text-3xl lg:text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">
                   Preview
                 </h1>
                 <h2 className="mb-4 text-1xl lg:text-3xl tracking-tight font-bold text-gray-900 dark:text-white">
-                  Heading
+                  {title}
                 </h2>
                 <p className="font-light text-gray-500 sm:text-xl dark:text-gray-400">
-                  Description
+                  {description}
                 </p>
                 <img
                   src={URL.createObjectURL(image)}
@@ -104,13 +124,13 @@ const AddPost = () => {
                   className="mt-5 h-auto w-auto rounded"
                 />
               </div>
-            </article>          
-        </div>
+            </article>
+          </div>
         )}
       </div>
     </DefaultLayout>
   );
-  
+
 };
 
 export default AddPost;
